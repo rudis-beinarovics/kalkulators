c0e8925b6586c8007f7139b7ae932b725abac837
rudis-beinarovics <97430467+rudis-beinarovics@users.noreply.github.com>
2022-02-18 14:17:50 +0200

    Update kalkulators.py

--- kalkulators.py
+++ kalkulators.py
@@ -1,6 +1,29 @@
 from tkinter import*
 mansLogs=Tk()
 mansLogs.title("kalkulators")
-mansLogs.geometry("300x300")
-poga=Button(mansLogs, text="Sveiki!",bg="green", fg="white")
-mansLogs.mainloop
+
+btn0=Button(mansLogs, text=0, padx="40",pady=20)
+btn1=Button(mansLogs, text=1, padx="40",pady=20)
+btn2=Button(mansLogs, text=2, padx="40",pady=20)
+btn3=Button(mansLogs, text=3, padx="40",pady=20)
+btn4=Button(mansLogs, text=4, padx="40",pady=20)
+btn5=Button(mansLogs, text=5, padx="40",pady=20)
+btn6=Button(mansLogs, text=6, padx="40",pady=20)
+btn7=Button(mansLogs, text=7, padx="40",pady=20)
+btn8=Button(mansLogs, text=8, padx="40",pady=20)
+btn9=Button(mansLogs, text=9, padx="40",pady=20)
+
+btn7.grid(row=1,column=0)
+btn8.grid(row=1,column=1)
+btn9.grid(row=1,column=2)
+
+btn4.grid(row=2,column=0)
+btn5.grid(row=2,column=1)
+btn6.grid(row=2,column=2)
+
+btn1.grid(row=3,column=0)
+btn2.grid(row=3,column=1)
+btn3.grid(row=3,column=2)
+
+
+mansLogs(mainloop)
